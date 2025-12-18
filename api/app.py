4bdcbf6cb590d9be2008b31c6b65ddbfaa04ded8
harshithagmini-dev <harshithagmini@gmail.com>
2025-12-18 11:13:45 +0530

    Update app.py

--- api/app.py
+++ api/app.py
@@ -7,4 +7,4 @@ def home():
     return jsonify(message="Hello from DevOps Intern Assignment API (Flask)")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    app.run(host="0.0.0.0", port=5000)
